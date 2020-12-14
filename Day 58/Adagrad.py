ee1ceef8d58589e490d037d6362ca0ba8fe5e9ec
saurabhshahane4 <saurabhshahane4@gmail.com>
2020-12-14 23:17:58 +0530

    Adagrad

---
+++ Day 58/Adagrad.py
@@ -0,0 +1,35 @@
+from d2l import torch as d2l
+import math
+import torch
+
+def adagrad_2d(x1, x2, s1, s2):
+    eps = 1e-6
+    g1, g2 = 0.2 * x1, 4 * x2
+    s1 += g1 ** 2
+    s2 += g2 ** 2
+    x1 -= eta / math.sqrt(s1 + eps) * g1
+    x2 -= eta / math.sqrt(s2 + eps) * g2
+    return x1, x2, s1, s2
+
+def f_2d(x1, x2):
+    return 0.1 * x1 ** 2 + 2 * x2 ** 2
+
+eta = 0.4
+d2l.show_trace_2d(f_2d, d2l.train_2d(adagrad_2d))
+
+def init_adagrad_states(feature_dim):
+    s_w = torch.zeros((feature_dim, 1))
+    s_b = torch.zeros(1)
+    return (s_w, s_b)
+
+def adagrad(params, states, hyperparams):
+    eps = 1e-6
+    for p, s in zip(params, states):
+        with torch.no_grad():
+            s[:] += torch.square(p.grad)
+            p[:] -= hyperparams['lr'] * p.grad / torch.sqrt(s + eps)
+        p.grad.data.zero_()
+
+data_iter, feature_dim = d2l.get_data_ch11(batch_size=10)
+d2l.train_ch11(adagrad, init_adagrad_states(feature_dim),
+               {'lr': 0.1}, data_iter, feature_dim);
