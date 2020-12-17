79fc7685c8795de197353b96d0f4fa0a27b40eb3
saurabhshahane4 <saurabhshahane4@gmail.com>
2020-12-17 18:45:08 +0530

    Adam

---
+++ Day 60/Adam.py
@@ -0,0 +1,28 @@
+from d2l import torch as d2l
+import torch
+
+def init_adam_states(feature_dim):
+    v_w, v_b = torch.zeros((feature_dim, 1)), torch.zeros(1)
+    s_w, s_b = torch.zeros((feature_dim, 1)), torch.zeros(1)
+    return ((v_w, s_w), (v_b, s_b))
+
+def adam(params, states, hyperparams):
+    beta1, beta2, eps = 0.9, 0.999, 1e-6
+    for p, (v, s) in zip(params, states):
+        with torch.no_grad():
+            v[:] = beta1 * v + (1 - beta1) * p.grad
+            s[:] = beta2 * s + (1 - beta2) * torch.square(p.grad)
+            v_bias_corr = v / (1 - beta1 ** hyperparams['t'])
+            s_bias_corr = s / (1 - beta2 ** hyperparams['t'])
+            p[:] -= hyperparams['lr'] * v_bias_corr / (torch.sqrt(s_bias_corr)
+                                                       + eps)
+        p.grad.data.zero_()
+    hyperparams['t'] += 1
+
+
+data_iter, feature_dim = d2l.get_data_ch11(batch_size=10)
+d2l.train_ch11(adam, init_adam_states(feature_dim),
+               {'lr': 0.01, 't': 1}, data_iter, feature_dim);
+
+trainer = torch.optim.Adam
+d2l.train_concise_ch11(trainer, {'lr': 0.01}, data_iter)
